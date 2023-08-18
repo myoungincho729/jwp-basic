778f791cd8655c485466483d684ad82b99e122ef
myoungin <myoungin729@gmail.com>
2023-08-18 15:05:31 +0900

    refactor: 하나의 jdbcTemplate을 사용하도록 통합

---
+++ src/main/java/next/dao/JdbcTemplate.java
@@ -0,0 +1,34 @@
+package next.dao;
+
+import core.jdbc.ConnectionManager;
+import next.model.User;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
+public abstract class JdbcTemplate {
+    public void update(User user) throws SQLException {
+        // TODO 구현 필요함.
+        Connection con = null;
+        PreparedStatement pstmt = null;
+        try {
+            con = ConnectionManager.getConnection();
+            String sql = createQuery();
+            pstmt = con.prepareStatement(sql);
+            setValues(user, pstmt);
+
+            pstmt.executeUpdate();
+        } finally {
+            if (pstmt != null) {
+                pstmt.close();
+            }
+
+            if (con != null) {
+                con.close();
+            }
+        }
+    }
+    public abstract String createQuery();
+    public abstract void setValues(User user, PreparedStatement pstmt) throws SQLException;
+}
