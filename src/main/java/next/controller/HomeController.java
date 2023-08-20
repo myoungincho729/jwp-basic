c25d94319b91e2ea738c3206cd552934854e1b91
myoungin <myoungin729@gmail.com>
2023-08-20 16:16:00 +0900

    feat: HomeController 질문 리스트 받는 것으로 수정

--- src/main/java/next/controller/HomeController.java
+++ src/main/java/next/controller/HomeController.java
@@ -4,13 +4,18 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import core.mvc.Controller;
+import next.dao.QuestionDao;
 import next.dao.UserDao;
+import next.model.Question;
+
+import java.util.List;
 
 public class HomeController implements Controller {
     @Override
     public String execute(HttpServletRequest req, HttpServletResponse resp) throws Exception {
-        UserDao userDao = new UserDao();
-        req.setAttribute("users", userDao.findAll());
+        QuestionDao questionDao = new QuestionDao();
+        List<Question> questions = questionDao.findAll();
+        req.setAttribute("questions", questions);
         return "home.jsp";
     }
 }
