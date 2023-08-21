53c3a2ed916d028ebd5a605b8d99fcee9c676ea9
myoungin <myoungin729@gmail.com>
2023-08-21 11:39:40 +0900

    feat: 9번 문제 - 질문 리스트 json 데이터로 반환하는 컨트롤러 작성

--- src/main/java/core/mvc/RequestMapping.java
+++ src/main/java/core/mvc/RequestMapping.java
@@ -36,6 +36,7 @@ public class RequestMapping {
         mappings.put("/qna/show", new ShowController());
         mappings.put("/api/qna/addAnswer", new AddAnswerController());
         mappings.put("/api/qna/deleteAnswer", new DeleteAnswerController());
+        mappings.put("/api/qna/list", new ListQuestionController());
 
         logger.info("Initialized Request Mapping!");
     }
