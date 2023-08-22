faa1a5885cfcfe8ca1203e8d6fc77a61bc061ef7
myoungin <myoungin729@gmail.com>
2023-08-22 15:18:03 +0900

    refactor: try-catch 사용, 필드값을 가지지 않게 변경

--- src/main/java/core/nmvc/ControllerScanner.java
+++ src/main/java/core/nmvc/ControllerScanner.java
@@ -2,31 +2,31 @@ package core.nmvc;
 
 import core.annotation.Controller;
 import org.reflections.Reflections;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.util.HashMap;
 import java.util.Map;
 import java.util.Set;
 
 public class ControllerScanner {
+    private static final Logger logger = LoggerFactory.getLogger(ControllerScanner.class);
     private Reflections reflections;
-    private Set<Class<?>> annotated;
-    private Map<Class<?>, Object> instances = new HashMap<>();
 
-    public ControllerScanner(Object... basePackages) throws InstantiationException, IllegalAccessException {
+    public ControllerScanner(Object... basePackages) {
         this.reflections = new Reflections(basePackages);
-        annotated = reflections.getTypesAnnotatedWith(Controller.class);
-        for (Class clazz : annotated) {
-            instances.put(clazz, clazz.newInstance());
-        }
     }
 
-    public void printControllers() {
-        for (Class clazz : annotated) {
-            System.out.println("clazz.getName() = " + clazz.getName());
+    public Map<Class<?>, Object> getControllers() {
+        Set<Class<?>> annotated = reflections.getTypesAnnotatedWith(Controller.class);
+        Map<Class<?>, Object> instances = new HashMap<>();
+        try {
+            for (Class clazz : annotated) {
+                instances.put(clazz, clazz.newInstance());
+            }
+        } catch (InstantiationException | IllegalAccessException e) {
+            logger.error(e.getMessage());
         }
-    }
-
-    public Map<Class<?>, Object> getInstances() {
         return instances;
     }
 }
--- src/test/java/core/nmvc/ControllerScannerTest.java
+++ src/test/java/core/nmvc/ControllerScannerTest.java
@@ -1,12 +1,24 @@
 package core.nmvc;
 
 import junit.framework.TestCase;
+import org.junit.Before;
 import org.junit.Test;
 
+import java.util.Map;
+
 public class ControllerScannerTest {
+    private ControllerScanner cs;
+    @Before
+    public void setup() {
+        cs = new ControllerScanner("core.nmvc");
+    }
+
     @Test
-    public void scannerTest() throws InstantiationException, IllegalAccessException {
-        ControllerScanner controllerScanner = new ControllerScanner("core.nmvc");
-        controllerScanner.printControllers();
+    public void scannerTest() {
+        Map<Class<?>, Object> controllers = cs.getControllers();
+        for (Class controller : controllers.keySet()) {
+            System.out.println("controller = " + controller);
+            System.out.println("controllers.get(controller) = " + controllers.get(controller));
+        }
     }
 }
